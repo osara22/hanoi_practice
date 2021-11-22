492c954792bb6b16632d1a34d55aed9ca592db1a
osara22 <osaraiya22@gmail.com>
2021-11-23 02:07:15 +0900

    完成

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-
+let left = [];
+let center = [];
+let right = [];
+let count = 1;
+let size = 5;
 
 function run() {
-  hello("hello");
+  for(var i = 1; i <= size; i++){
+    left.push(i);
+  }
+  console.log("1: left=" + left + " center=" + center + " right=" + right);
+  hanoi(size, left, center, right);
 }
 
-const hello = (word) => {
-  return 0;
+const hanoi = (size, from, work, target) => {
+  if(size < 1) return;
+  hanoi(size-1, from, target, work);
+  // console.log("円盤の大きさ" + size + "を" + from + "から" + work + "に動かします");
+  from.shift();
+  work.unshift(size);
+  count++;
+  console.log(count + ": left=" + left + " center=" + center + " right=" + right);
+  hanoi(size-1, target, work, from);
 }
